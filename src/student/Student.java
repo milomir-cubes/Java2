ffdadc92d52f646dcfaa33bcbdd10698454bbf3b
Milomir <milomir.dragovic@cubes.rs>
2018-06-08 16:52:31 +0200

    Adds 2 new attributes to class Student (Course, Computer)

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,12 +1,17 @@
 
 package student;
 
+import computer.Computer;
+import course.Course;
+
 
 public class Student {
     
     private String firstName;
     private String lastName;
     private int yearOfBirth;
+    private Course course;
+    private Computer computer;
 
     public Student() {
         this.firstName = "not defined";
@@ -19,6 +24,34 @@ public class Student {
         this.firstName = firstName;
         this.lastName = lastName;
         this.yearOfBirth = yearOfBirth;
+    }
+
+    public Student(String firstName, String lastName, int yearOfBirth, Course course, Computer computer) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+        this.course = course;
+        this.computer = computer;
+    }
+
+    
+    
+    public Computer getComputer() {
+        return computer;
+    }
+
+    public void setComputer(Computer computer) {
+        this.computer = computer;
+    }
+    
+    
+    
+    public Course getCourse() {
+        return this.course;
+    }
+    
+    public void setCourse(Course course) {
+        this.course = course;
     }
 
     public String getFirstName() {
