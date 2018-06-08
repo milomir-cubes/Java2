1e917522edacc54a4dfd784b8a57b1934698c9ae
Milomir <milomir.dragovic@cubes.rs>
2018-06-08 16:43:52 +0200

    Adds Course class

---
+++ src/course/Course.java
@@ -0,0 +1,38 @@
+
+package course;
+
+
+public class Course {
+    
+    private String name;
+    private int numberOfClasses;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfClasses() {
+        return numberOfClasses;
+    }
+
+    public void setNumberOfClasses(int numberOfClasses) {
+        this.numberOfClasses = numberOfClasses;
+    }
+
+    public Course() {
+        this.name = "no name";
+        this.numberOfClasses = 0;
+    }
+
+    public Course(String name, int numberOfClasses) {
+        this.name = name;
+        this.numberOfClasses = numberOfClasses;
+    }
+    
+    
+    
+}
